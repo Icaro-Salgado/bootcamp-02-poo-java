7e2cf05022f56d035e5edbd95ca62d8a46401b18
Icaro-Salgado <icaro.salgado@mercadolivre.com>
2022-03-13 00:27:19 -0300

    refact: melhorando menus e navegação

--- src/corrida_na_selva/Main.java
+++ src/corrida_na_selva/Main.java
@@ -14,7 +14,45 @@ public class Main {
         this.evento = new Evento();
     }
 
+    public static void clearScreen() {  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }
+
+    private void printBanner() {
+        System.out.println();
+        System.out.println("a88888b.                            oo       dP");
+        System.out.println("d8'   `88                                     88");
+        System.out.println("88        .d8888b. 88d888b. 88d888b. dP .d888b88 .d8888b.");
+        System.out.println("88        88'  `88 88'  `88 88'  `88 88 88'  `88 88'  `88");
+        System.out.println("Y8.   .88 88.  .88 88       88       88 88.  .88 88.  .88");
+        System.out.println(" Y88888P' `88888P' dP       dP       dP `88888P8 `88888P8");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("                                       dP");
+        System.out.println("                                       88");
+        System.out.println("88d888b. .d8888b.    .d8888b. .d8888b. 88 dP   .dP .d8888b.");
+        System.out.println("88'  `88 88'  `88    Y8ooooo. 88ooood8 88 88   d8' 88'  `88");
+        System.out.println("88    88 88.  .88          88 88.  ... 88 88 .88'  88.  .88");
+        System.out.println("dP    dP `88888P8    `88888P' `88888P' dP 8888P'   `88888P8");
+        System.out.println();
+    }
+
+    private void printMenuTitle(String title) {
+        // Tamanho do banner, pode ser obtido contando o tamanho da maior linha do banner
+        int bannerSize = 59;
+        
+        // Espaços entre o inicio do titulo || e o texto do titulo
+        // a largura do titulo do menu deve ser igual a do banner
+        int spacesBetween = Math.floorDiv((bannerSize - title.length() - 4), 2);
+
+        System.out.println("=".repeat(bannerSize));
+        System.out.println("||" + " ".repeat(spacesBetween) + title + " ".repeat(spacesBetween) + " ||");
+        System.out.println("=".repeat(bannerSize));
+    }
+
     private void printMenuPrincipal() {
+        // A partir daqui o código é unicamente para printar melhor no cli
         String[] opcoes = new String[] {
                 "1 - Adicionar inscrição",
                 "2 - Mostrar todas inscrições",
@@ -23,15 +61,18 @@ public class Main {
                 "5 - Registrar pagamento",
                 "6 - Sair do programa"
         };
-
-        System.out.println("\nMenu principal");
-        System.out.println("=================\n");
+        
+        this.printBanner();
+        this.printMenuTitle("Menu principal");
+        // System.out.println("=".repeat(59));
+        // System.out.println("||" + " ".repeat(20) + "Menu principal" + " ".repeat(20) + " ||");
+        // System.out.println("=".repeat(59));
         for (String opcao : opcoes) {
             System.out.println(opcao);
         }
     }
 
-    private void printMenuSelecionarCategoria() {
+    private void printMenuSelecionarCategoria(boolean horizontal) {
         String[] opcoes = new String[] {
                 "1 - Circuito pequeno",
                 "2 - Circuito médio",
@@ -40,8 +81,14 @@ public class Main {
 
         System.out.println("\nSelecione a categoria:");
         for (String opcao : opcoes) {
-            System.out.println(opcao);
+            if (horizontal){ 
+                System.out.print(opcao + "\t");
+            } else {
+                System.out.println(opcao);
+            }
         }
+
+        System.out.println();
     }
 
     private Integer getUserMainMenuInput() {
@@ -62,12 +109,14 @@ public class Main {
     }
 
     private void inscreverParticipante() {
+        this.printBanner();
+        this.printMenuTitle("Adicionar participante");
 
         System.out.print("Digite o nome do participante: ");
         input.nextLine();
         String nomeParticipante = input.nextLine();
 
-        printMenuSelecionarCategoria();
+        printMenuSelecionarCategoria(true);
         Integer userInput = input.nextInt();
         Categoria categoria;
 
@@ -84,7 +133,7 @@ public class Main {
     private void getParticipantesNaCategoria() {
         Integer opcao;
 
-        printMenuSelecionarCategoria();
+        printMenuSelecionarCategoria(false);
         System.out.print("Digite a opção desejada: ");
 
         opcao = input.nextInt();
@@ -142,18 +191,22 @@ public class Main {
 
             switch (userOption) {
                 case 1:
+                    clearScreen();
                     this.inscreverParticipante();
                     break;
 
                 case 2:
+                    clearScreen();
                     this.evento.listarTodasInscrições();
                     break;
 
                 case 3:
+                    clearScreen();
                     this.getParticipantesNaCategoria();
                     break;
 
                 case 4:
+                    clearScreen();
                     this.cancelarInscricao();
                     break;
 
