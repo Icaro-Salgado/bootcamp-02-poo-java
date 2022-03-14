535045a02bdefdc5f989fa3b100bddb985e92240
Icaro-Salgado <icaro.salgado@mercadolivre.com>
2022-03-14 11:24:24 -0300

    adicionando novo exercicio

--- src/parte_02_pratica_integradora_2_exercicio_1/Main.java
+++ src/parte_02_pratica_integradora_2_exercicio_1/Main.java
@@ -1,4 +1,4 @@
-package parte_02_pratica_integradora_2;
+package parte_02_pratica_integradora_2_exercicio_1;
 
 public class Main {
     
--- src/parte_02_pratica_integradora_2_exercicio_1/PraticaExcecoes.java
+++ src/parte_02_pratica_integradora_2_exercicio_1/PraticaExcecoes.java
@@ -1,4 +1,4 @@
-package parte_02_pratica_integradora_2;
+package parte_02_pratica_integradora_2_exercicio_1;
 
 public class PraticaExcecoes {
     private static final int A=0, B=300;
